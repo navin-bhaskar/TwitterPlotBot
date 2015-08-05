c727fb118e2950a272a017106feb3150e6b34d91
Navin Bhaskar <navin.bhaskar.5@gmial.com>
2015-08-05 09:58:21 +0000

    Removed my screen name from config file

--- config.py
+++ config.py
@@ -4,14 +4,14 @@
 config = {
     "Tweet Interval": 60*2,  # Tweet interval in seconds; 60*60 makes it one hour
     "Log Interval": 60,     # Data logging interval in seconds 60*5 makes it 5 minutes 
-    "Twitter handle": "@navin_bhaskar", # Tweet handle to be used while tweeting
+    "Twitter handle": "@your screen name", # Tweet handle to be used while tweeting
     "xlabel":"Time",   # X label
     "ylabel":"Data",   # Y axis label
     "title": "Plot FTW", # Title of the plot
-    "consumer key": 'consumer key',
-    "consumer secret": 'consumer secret',
-    "access token": 'access token',
-    "access token secret": 'acccess token secret',
+    "consumer key": 'consumer key here',
+    "consumer secret": 'consumer secret here',
+    "access token": 'access token here',
+    "access token secret": 'access token secret here',
     "stop plot cmd": "#stoptheplot",
     "resume plot cmd": "#resumeplot",
     "screen id": "@navin_bhaskar",
